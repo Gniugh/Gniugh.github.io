72b6144669059d55ddb8626314164935560ce980
gniugh <2546278958@qq.com>
2024-11-15 20:43:56 +0800

    Site updated: 2024-11-15 20:43:56

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1731674465858";
+const VERSION = "1731674635837";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
